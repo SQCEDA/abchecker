f1e844a479e7d11ce2cdfdaf9dad8eed0552dba8
YouWei Zhao <zhao.uv@gmail.com>
2020-11-05 19:55:29 +0800

    calculateImageInformation

--- src/calculateImageInformation.js
+++ src/calculateImageInformation.js
@@ -1,8 +1,31 @@
-const { solve } = require('./fitTransform')
+const { solve, getPixelPositionFromMNPQ } = require('./fitTransform')
 const { getImageInformationFromDir } = require('./imageInformation')
+const fs = require('fs')
 
 function processOneDir(data, workDir, imageInformationBlock, index) {
     const { width, height, col, row, files } = getImageInformationFromDir(imageInformationBlock.dir)
+    let output = { files }
+    let config = imageInformationBlock.config
+    if (imageInformationBlock.positionTransfrom.type === 'positionPairs') {
+        let x, y, u, v, m, n, p, q;
+        let objs = imageInformationBlock.positionTransfrom.positionPair
+        x = objs.map(v => v.x)
+        y = objs.map(v => v.y)
+        m = objs.map(v => v.m)
+        n = objs.map(v => v.n)
+        p = objs.map(v => v.p)
+        q = objs.map(v => v.q)
+        let uv = objs.map((v, i) => getPixelPositionFromMNPQ(m[i], n[i], p[i], q[i], width, height))
+        u = uv.map(v => v[0])
+        v = uv.map(v => v[1])
+        Object.assign(output, { x, y, m, n, p, q })
+        Object.assign(output, solve(x, y, u, v))
+    } else {
+        let { A, B, C, D, E, F } = imageInformationBlock.positionTransfrom
+        Object.assign(output, { A, B, C, D, E, F })
+    }
+    fs.writeFileSync(config, JSON.stringify(output), { encoding: 'utf8' })
+    return output
 }
 
 function calculateImageInformation(data) {
@@ -10,6 +33,7 @@ function calculateImageInformation(data) {
     let infos = data.imageInformation.map((imageInformationBlock, index) => {
         processOneDir(data, workDir, imageInformationBlock, index)
     });
+    return infos
 }
 
 exports.calculateImageInformation = calculateImageInformation
